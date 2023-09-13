72f1e6c362f13807168170768f50105360f4eb3f
jasminneenf <nazhifah.firyal.jasmine-2022@ftmm.unair.ac.id>
2023-09-13 17:13:08 +0700

    initial commit helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,13 @@
-list = [1, 2, 3, 4, 5, 32]
+jarak = float(input("Masukkan jarak pengiriman: "))
 
-if 32 in list:
-    print("True")
-
-print ("Hello World" * list[1])
+if jarak < 5:
+    print("Biaya ongkos kirim anda gratis")
+elif 5 <=  jarak <= 10:
+    print("Biaya ongkos kirim anda adalah: ", 5000*jarak)
+elif 10 <=  jarak <= 20:
+    print("Biaya ongkos kirim anda adalah: ", 6000*jarak)
+elif 20 <= jarak <= 30:
+    print("Biaya Ongkos Kirim anda adalah: ", 7000* jarak)   
+else:
+    print("Biaya ongkos kirim anda adalah: ", 9000*jarak) 
+    hf
